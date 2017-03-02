0f52d8d3ae7a0d01ea43bde1c245d0ae37723d4d
Jan Mozgawa <jmozgawa@gmail.com>
2017-03-02 12:53:10 +0100

    add markers layer to google map

--- src/containers/TravelMap.js
+++ src/containers/TravelMap.js
@@ -1,17 +1,52 @@
-import React, { Component } from 'react';
-import Map from 'google-maps-react';
+import React, {Component} from 'react';
+import Map, {Marker}  from 'google-maps-react';
+import MapMarkers from '../components/MapMarkers'
+
 import GoogleApiComponent from '../libs/GoogleApiComponent';
+
+
+const mockedMarkers = [
+  {
+    name: 'Dolores park',
+    position: {
+      lat: 37.759703,
+      lng: -122.428093
+    }
+  },
+  {
+    name: 'SOMA',
+    position: {
+      lat: 37.778519,
+      lng: -122.405640
+    }
+  },
+
+]
 
 
 export class TravelMap extends Component {
 
   render() {
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <Map google={this.props.google} />
+      <Map google={this.props.google}
+           className={'map'}>
+        {
+          mockedMarkers.map(marker =>
+
+            <Marker name={marker.name} position={marker.position} />
+          )
+        }
+
+
+      </Map>
     );
   }
 }
 
 export default GoogleApiComponent({
-   apiKey: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo"
- })(TravelMap)
+  apiKey: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo"
+})(TravelMap)
