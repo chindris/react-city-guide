9a840af193d4c5e746f5b3bdad7da2b5a03b56bb
Jan Mozgawa <jmozgawa@gmail.com>
2017-03-02 12:55:06 +0100

    refactor

--- src/containers/TravelMap.js
+++ src/containers/TravelMap.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Map, {Marker}  from 'google-maps-react';
-import MapMarkers from '../components/MapMarkers'
 
 import GoogleApiComponent from '../libs/GoogleApiComponent';
 
@@ -40,8 +39,6 @@ export class TravelMap extends Component {
             <Marker name={marker.name} position={marker.position} />
           )
         }
-
-
       </Map>
     );
   }
