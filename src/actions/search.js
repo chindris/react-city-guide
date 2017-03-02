cd4a617e8e065e0aa8d4247dcf6b23e8df858a10
lexiht <hazukitran@gmail.com>
2017-03-02 12:20:43 +0000

    Fixed syntax errors

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export const search(value) {
+export function search(value) {
   return {
     type: 'SEARCH',
     value,
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import AttractionsList from './List/AttractionsList';
 import logo from '../logo.svg';
 import TravelMap from './TravelMap';
-import SearchBar from './Search/SearchBar';
+import SearchBar from './List/SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <AttractionsList />
-        <TravelMap /><br /> */}
         <SearchBar />
+        <AttractionsList />
+        {/* <TravelMap /><br /> */}
         {/* search bar need to be in header */}
       </div>
     );
