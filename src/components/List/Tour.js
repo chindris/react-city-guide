e238774d2fd41135ffd2a7a109b82d58f7067dbf
Vasile Chindris <vasile.chindris@amazee.com>
2017-03-03 16:12:39 +0100

    Make the selected tour visible.

--- src/components/List/Tour.js
+++ src/components/List/Tour.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Tour = ({tour, onTourSelected}) => (
+const Tour = ({tour, onTourSelected, isSelected}) => (
   <Item onClick={() => onTourSelected(tour.id)}>
     <Image src={tour.image} />
     <div>
