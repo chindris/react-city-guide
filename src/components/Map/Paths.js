cde9e9ec3929a9815f2236f8e6267a842357edc0
Jan Mozgawa <jmozgawa@gmail.com>
2017-03-03 18:06:48 +0100

    draw real path from google direction service

--- src/components/Map/Paths.js
+++ src/components/Map/Paths.js
@@ -2,23 +2,64 @@ import React, {Component} from 'react';
 
 
 export class Paths extends Component {
-  render () {
+
+  constructor(props) {
+    super(props);
+    this.directionsService = new window.google.maps.DirectionsService;
+    this.directionsDisplay = new window.google.maps.DirectionsRenderer({
+      draggable: true,
+      map: this.props.map,
+      panel: document.getElementById('right-panel')
+    });
+  }
+
+  displayRoute = (route) => {
+
+    const waypoints = route.length > 2 ? route.slice(1, route.length - 2).map((point) => {
+      return {
+        location: point,
+        stopover: true
+      };
+    }) : null;
+
+    this.directionsService.route({
+      origin: route[0],
+      waypoints: waypoints,
+      destination: route[route.length - 1],
+      travelMode: 'WALKING',
+      avoidTolls: true
+    }, (response, status) => {
+      if (status === 'OK') {
+        console.log("JMOZGAWA: response", response);
+        this.directionsDisplay.setDirections(response);
+
+        const path = response.routes[0].overview_path;
+        //
+        var flightPath = new window.google.maps.Polyline({
+          path: path,
+          strokeColor: "#FF0000",
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+
+        console.log("JMOZGAWA: flightPlanCoordinates", this.props.flightPlanCoordinates);
+
+
+        flightPath.setMap(this.props.map);
+
+
+      } else {
+        alert('Could not display directions due to: ' + status);
+      }
+    });
+  }
+
+  render() {
     console.log("JMOZGAWA: this", this);
 
-    var flightPath = new window.google.maps.Polyline({
-      path: this.props.flightPlanCoordinates,
-      strokeColor: "#FF0000",
-      strokeOpacity: 1.0,
-      strokeWeight: 2
-    });
+    this.displayRoute(this.props.flightPlanCoordinates);
 
-
-
-    console.log("JMOZGAWA: flightPlanCoordinates", this.props.flightPlanCoordinates);
-
-
-
-    flightPath.setMap(this.props.map);
 
     return null;
   }
